bd7b8a7a0b4ca0cd7a9bf580425d7eac749a31a2
Aleksey5556 <96414248+Aleksey5556@users.noreply.github.com>
2021-12-24 21:26:32 +0300

    Initial commit

---
+++ src/main/java/geekBrains/Work.java
@@ -0,0 +1,57 @@
+package geekBrains;
+
+public class Work {
+   /* public static void main(String[] args) {
+
+        within10and20();
+    }
+
+    public static void within10and20() {
+        int a = 5;
+        int b = 6;
+        int x = a + b;
+        boolean n = x >= 10 && x <= 20 ;
+     System.out.println(n);
+
+        }*/
+
+
+    /* public static void main(String[] args) {
+         isPositiveOrNegative();
+     }
+
+     public static void isPositiveOrNegative() {
+         int a =5;
+         int b = -20;
+         int summ = a + b;
+         if (summ >= 0) {
+             System.out.println("+");
+         } else {
+             System.out.println("-");
+         }
+     }*/
+   /* public static void main(String[] args) {
+        isNegative();
+    }
+
+    public static void isNegative() {
+        int a = 15;
+
+boolean i = a >= 0;
+        System.out.println(i);
+    }*/
+    public static void main(String[] args) {
+        printWordNTimes();
+    }
+    public static void printWordNTimes() {
+        for (int n =0; n < 10; n++ ) {
+            System.out.println("85");
+        }
+    }
+
+
+
+
+}
+
+
